77f8246192dbb1f845afb85f539292b32cc07aa5
Charles Allison <soprinye@gmail.com>
2021-02-26 09:54:35 +0100

    Get static page to display via lambda api call

---
+++ big-mouth/functions/get-index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const fs = require('fs');
+
+var html;
+
+function loadHtml() {
+  if(!html) {
+    html = fs.readFileSync('static/index.html', 'utf-8');
+  }
+  return html;
+}
+
+module.exports.handler = async (event) => {
+  try {
+    let html = loadHtml();
+
+    const response = {
+      statusCode: 200,
+      body: html,
+      headers: {
+        'Content-Type' : 'text/html; charset=UTF-8'
+      }
+    };
+
+    return response;
+  }catch(error) {
+    console.log(error);
+  }
+
+};
