c6f12043f2dc23fd2646825f0912ac0c2a0a1f20
Charles Allison <soprinye@gmail.com>
2021-02-27 12:12:08 +0100

    Modify file name getRestaurants

---
+++ big-mouth/functions/getRestaurants.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const AWS = require('aws-sdk')
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+const defaultRestaurants = process.env.defaultRestaurants || 8;
+const tableName = process.env.restaurants_table;
+
+const getRestaurants = {};
+
+getRestaurants.getAll = async (count) => {    
+    return new Promise((resolve, reject) => {
+        const req = {
+            TableName: tableName,
+            Limit: count
+        }
+        
+        dynamodb.scan(req, (err, data) => {
+            if(err) reject(err);
+            else resolve(data);
+        });
+    });
+};
+
+getRestaurants.handler = async (event) => {
+    const restaurants = await getRestaurants.getAll(defaultRestaurants);
+    console.log(restaurants);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(restaurants)
+    }
+};
+
+module.exports = getRestaurants;
